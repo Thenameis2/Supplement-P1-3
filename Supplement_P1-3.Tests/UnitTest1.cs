abcac2675187e4ee6bfdddee1cb78de0acc4aed6
Mihiretu Jackson <mihiretujackson@gmail.com>
2025-02-06 16:28:27 -0500

    test: added unit tests for first method

--- Supplement_P1-3.Tests/UnitTest1.cs
+++ Supplement_P1-3.Tests/UnitTest1.cs
@@ -2,9 +2,19 @@
 
 public class UnitTest1
 {
+
+    private readonly string testAsciiFile = "test_ascii.txt";
+    
     [Fact]
-    public void Test1()
+    public void CreateAsciiFile()
     {
+         string content = "Hello, File!";
+        Supplement_P1_3.CreateAsciiFile(testAsciiFile, content);
+        
+        Assert.True(File.Exists(testAsciiFile));
+        Assert.Equal(content, File.ReadAllText(testAsciiFile));
+
+        File.Delete(testAsciiFile);
 
     }
 }
