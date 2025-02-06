c8cbfee3e9bf0eca137519213225ba277372a617
Mihiretu Jackson <mihiretujackson@gmail.com>
2025-02-06 16:40:33 -0500

    doc: added documentation for CreateAsciiFile function

--- Supplement_P1-3/Supplement P1-3.cs
+++ Supplement_P1-3/Supplement P1-3.cs
@@ -2,7 +2,12 @@
 
 public class Supplement_P1_3
 {
-
+    
+       /// <summary>
+       /// Creates an ASCII file that takes take two parameters
+       /// </summary>
+       /// <param name="filePath"> The path where the file will be created </param>
+       /// <param name="content"> The text content to write to the file.</param>
        public static void CreateAsciiFile(string filePath, string content)
         {
            File.WriteAllText(filePath, content);
